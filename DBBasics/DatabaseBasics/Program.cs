28b733ed7aefebffbabb613e0d65313b9ea47221
Tiago Nunes <ntiagon@gmail.com>
2021-11-13 17:17:57 -0300

    fixed link

---
+++ DBBasics/DatabaseBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+string connectionString = @"Data Source=DESKTOP-080VCN2;
+Database=ExampleOne;
+User ID=DESKTOP-080VCN2\feed_;
+Password=;
+Trusted_Connection=Yes;";
+
+SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+Debug.WriteLine("Connected to the server!");
+SqlCommand cmd = new SqlCommand();
+cmd.Connection = connection;
+cmd.CommandType = CommandType.Text;
+cmd.CommandText = "select * from dbo.People";
+SqlDataReader reader = cmd.ExecuteReader();
+if (reader.HasRows)
+{
+    while (reader.Read())
+    {
+        Debug.WriteLine(reader.GetString(1) + " - " + reader.GetString(2));
+    }
+}
+connection.Close();
--- MasterClass/ConsoleApp1/Program.cs
+++ MasterClass/ConsoleApp1/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections;
 
 namespace ConsoleApp1
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Employee
     {
-      Console.WriteLine("Teste");
+        public int id;
     }
-  }
+    internal class Program : IComparer
+    {
+        private static void Main(string[] args)
+        {
+           
+        }
+
+        public int Compare(object x, object y)
+        {
+            Employee employee1 = (Employee)x;
+            Employee employee2 = (Employee)y;
+            return employee1.id.CompareTo(employee2.id);
+        }
+    }
 }
